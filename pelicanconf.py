c7788f142cde46e567863c1fd97bcb583a8897fc
convergenceIM <cgray@convergence-investments.com>
2019-12-15 23:35:01 -0800

    updated css for multi-authors

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'Chad Gray'
+#AUTHOR = 'Chad Gray'
 SITENAME = 'The Alpha Scientist'
 SITEURL = 'https://alphascientist.com'
 
